f36992ce32dd82cca9281c8ab0f01e1f22d84b2f
gamakichiivanyaroslavovich <78366400+gamakichiivanyaroslavovich@users.noreply.github.com>
2021-02-15 23:45:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+  const swiperindex = new Swiper('.swaper-index', {
+    // параметры салйдера
+    loop: true,
+    slidesPerView: 1,
+    speed: 1800,
+  
+    // Подключение стрелок
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  const swiperproject = new Swiper('.swiper-project', {
+    // параметры салйдера
+    loop: true,
+    slidesPerView: 1,
+    speed: 1800,
+  
+    // Подключение стрелок
+    navigation: {
+      nextEl: '.project__button__prev',
+      prevEl: '.project__button__next',
+    },
+  });
+});
